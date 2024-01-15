15332be72a73a9797eaed6a23730e79c3feddec2
Мадрахимова Дарья <d.madrahimova@mindsw.io>
2024-01-15 13:58:45 +0300

    fix build scripts

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
+set -e
+
+sudo apt update
+sudo apt install -y dpkg-dev devscripts python3 python3-pip python3-venv
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r ../requirements.txt
+
 SRC_PATH="rnf-backend"
 
-. changelog_gen.sh
-python3 -m pip install -r requirements.txt
+rm -rf "$SRC_PATH/spec" "$SRC_PATH/dist" "$SRC_PATH/build"
+pyinstaller --specpath "$SRC_PATH/spec" --distpath "$SRC_PATH/dist" --workpath "$SRC_PATH/build" ../main.py -n api
+
+CHANGELOG_PATH="$SRC_PATH/debian/changelog"
+CHANGELOG="$(cat $CHANGELOG_PATH)"
+
+if [ "$CHANGELOG" == "" ]; then
+  . changelog_gen.sh
+fi
+
+cd "$SRC_PATH" && dpkg-buildpackage -us -uc -b
--- packaging/changelog_gen.sh
+++ packaging/changelog_gen.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 VER="$1"
 DATE="$(date -R)"
-CHANGELOG_PATH="rnf-backend/DEBIAN/changelog"
+CHANGELOG_PATH="rnf-backend/debian/changelog"
 
 if [ "$VER" == "" ]; then
   VER=0.1
 fi
 
 INPUT=`sed "s/VER/$VER/g; s/DATE/$DATE/g" changelog_tmpl`
-echo -e "$INPUT\n\n$(cat $CHANGELOG_PATH)" > "$CHANGELOG_PATH"
+CHANGELOG="$(cat $CHANGELOG_PATH)"
+
+if [ "$CHANGELOG" != "" ]; then
+  INPUT="$INPUT\n\n"
+fi
+
+echo -e "$INPUT$(cat $CHANGELOG_PATH)" > "$CHANGELOG_PATH"
